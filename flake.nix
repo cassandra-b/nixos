4506ae6fc2c89bfd37d0b2ecf1d1e0733e5d6253
Cassie Bleskachek <cassie.bleskachek@gmail.com>
2024-09-01 08:28:25 -0500

    changing 23.11 to 24.05 so chrome will upgrade

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   inputs = {
     home-manager = {
       inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
     };
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
   outputs = { self, home-manager, nixpkgs }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
